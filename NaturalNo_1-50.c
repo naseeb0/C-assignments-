940109996ffea789f6b978f254f939537585e4ce
Naseeb <40313494+naseeb0@users.noreply.github.com>
2021-02-25 12:10:11 +0545

    Natural.C

---
+++ NaturalNo_1-50.c
@@ -0,0 +1,37 @@
+Using While 
+#include<stdio.h>
+int main(){
+    int i=1;
+    while (i<=50) {
+        printf("%d\n",i);
+        i=i+1;
+    }
+    
+return 0;
+}
+
+Using Do WHile
+
+
+#include<stdio.h>
+int main(){
+    int i=1;
+    do{
+        printf("%d\n",i);
+        i=i+1;
+    }
+    while(i<=50);
+    
+return 0;
+}
+
+
+Using For:
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=1;i<=50;i++){
+        printf("%d\n",i);
+    }
+return 0;
+}
